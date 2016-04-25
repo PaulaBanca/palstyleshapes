cf7a91a887497527caab24d6f5838f2a5709c3f4
Tom Piercy <tap32@medschl.cam.ac.uk>
2016-04-25 10:45:29 +0100

    Ensure the code is agnostic as to where it is running

--- init.lua
+++
@@ -1,5 +0,0 @@
-local fullpath=debug.getinfo(1,'S').source
-fullpath=fullpath:sub(2,-1)
-local last_slash_start=fullpath:find("/[^/]*$")
-local lua_subtree_path=fullpath:sub(1,last_slash_start-1)
-package.path=package.path..";"..lua_subtree_path.."/?.lua"
