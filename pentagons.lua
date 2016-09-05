e7eba15017165ea2b49676fd862fd3c7bcd67031
Tom Piercy <tap32@medschl.cam.ac.uk>
2016-09-05 18:04:51 +0100

    Fix image references

--- pentagons.lua
+++ pentagons.lua
@@ -10,14 +10,15 @@ local placer=require (folderOfThisFile.."placer")
 
 setfenv(1,M)
 
+local img=folderOfThisFile:sub(1,-2).."/img/palstyleshapes/pentagon.png"
 function create(w,h,max)
-  local t=display.newImage("img/palstyleshapes/pentagon.png")
+  local t=display.newImage(img)
   local mind=math.max(w/4,h/4)
   local mins=mind/t.width
   t:removeSelf()
 
   local function makeTriangles(x,y,w,h,angle)
-    local t=display.newImage("img/palstyleshapes/pentagon.png")
+    local t=display.newImage(img)
     local l=math.min(w,h)
     local s=math.max(mins,l/t.width)
     t:scale(s,s)
